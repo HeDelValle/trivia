a69bbf78318a875be8359a7137c36bba19e1e420
HeDelValle <72476230+HeDelValle@users.noreply.github.com>
2022-01-26 17:47:31 -0800

    Added questions and categories

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,3 +7,59 @@ function App() {
 }
 
 export default App;
+
+<App >
+  
+  <categories>
+//Music Marvel Animals  
+    <categories/>
+<Question >
+"Question goes here"
+//Which 2015 hit by The Weeknd begins, "And I know she'll be the death of me, at least we'll both be numb"?
+Can't Feel My Face
+
+What album was named the Best Rap Album at the 58th Annual Grammy Awards?
+To Pimp a Butterfly
+
+Whose song "No Role Modelz" was released in September 2014?
+J. Cole
+
+Eazy-E was a member of which rap group?
+N.W.A.
+
+Kanye West famously interrupted the acceptance speech of which singer at the 2009 MTV Video Music Awards?
+Taylor Swift
+
+//Marvel Questions
+What year was the first Iron Man movie released, kicking off the Marvel Cinematic Universe?
+2005
+2008 /Answer
+2010
+2012
+
+What is Captain America’s shield made of?
+Adamantium
+Vibranium /Answer
+Promethium
+Carbonadium
+
+Who was the last holder of the Space Stone before Thanos claims it for his Infinity Gauntlet?
+Thor
+Loki /Answer
+The Collector
+Tony Stark
+
+What song does Baby Groot dance to at the end of the first Guardian of the Galaxy?
+‘Cherry Bomb’ – The Runaways
+‘Ain’t No Mountain High Enough’ – Marvin Gaye & Tammi Terrell
+‘I Want You Back’ – The Jackson 5 /Answer
+‘Hooked On A Feeling’ – Voidoid
+
+What is Deadpool’s real name?
+Wade Wilson
+
+
+
+
+<Question />
+  <App />
